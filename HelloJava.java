6cb59156f65148d5082714b2c52c04c302076b4b
Vishaluv1234 <vishal.upadhyay@kritikalvision.ai>
2023-03-01 13:37:34 +0530

    Change comment

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,8 @@
 public class HelloJava {
 	
 	public static void main(String[] args) {
-
-		System.out.println("Hello Java new");
+			// Hello Line//
+		System.out.println("Hello Java new1121");
 		  
 		  
 	
